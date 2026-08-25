8fd3407cb49e7ba6c064b0f820433906cc864e33
Yutong Jiao <144712727+JaytonKogeru@users.noreply.github.com>
2026-08-25 09:59:35 +0800

    feat: expose broad job-pro bridge

--- campus_jobs/adapters/__init__.py
+++ campus_jobs/adapters/__init__.py
@@ -2,6 +2,7 @@ from .ashby import AshbyAdapter
 from .atsx import ATSXAdapter
 from .generic_browser import GenericBrowserAdapter
 from .greenhouse import GreenhouseAdapter
+from .jobpro_bridge import JobProBridgeAdapter
 from .lever import LeverAdapter
 from .moka import MokaAdapter
 from .oppo import OppoAdapter
@@ -11,6 +12,16 @@ from .wecruit import WecruitAdapter
 from .workday import WorkdayAdapter
 
 __all__ = [
-    "AshbyAdapter", "ATSXAdapter", "GenericBrowserAdapter", "GreenhouseAdapter", "LeverAdapter", "MokaAdapter",
-    "OppoAdapter", "SmartRecruitersAdapter", "VivoCampusAdapter", "WecruitAdapter", "WorkdayAdapter",
+    "ATSXAdapter",
+    "AshbyAdapter",
+    "GenericBrowserAdapter",
+    "GreenhouseAdapter",
+    "JobProBridgeAdapter",
+    "LeverAdapter",
+    "MokaAdapter",
+    "OppoAdapter",
+    "SmartRecruitersAdapter",
+    "VivoCampusAdapter",
+    "WecruitAdapter",
+    "WorkdayAdapter",
 ]
