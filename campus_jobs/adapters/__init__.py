677bebf5c7148f5b65dcfbbc396f91fd1ca7e88b
Yutong Jiao <144712727+JaytonKogeru@users.noreply.github.com>
2026-09-07 18:55:51 +1000

    wire Beisen zhiye adapter

--- campus_jobs/adapters/__init__.py
+++ campus_jobs/adapters/__init__.py
@@ -1,5 +1,6 @@
 from .ashby import AshbyAdapter
 from .atsx import ATSXAdapter
+from .beisen_zhiye import BeisenZhiyeAdapter
 from .generic_browser import GenericBrowserAdapter
 from .greenhouse import GreenhouseAdapter
 from .jobpro_bridge import JobProBridgeAdapter
@@ -16,6 +17,7 @@ from .workday import WorkdayAdapter
 __all__ = [
     "ATSXAdapter",
     "AshbyAdapter",
+    "BeisenZhiyeAdapter",
     "GenericBrowserAdapter",
     "GreenhouseAdapter",
     "JobProBridgeAdapter",
